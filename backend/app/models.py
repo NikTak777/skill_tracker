fd4111c9a1f85438abc77d3e33c20ba5147a4042
NikTak777 <nikkondrakhin@gmail.com>
2026-07-02 02:30:41 +0300

    Back-1 Добавлена кастомная модель User с ролями Manager/Employee

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        MANAGER = "manager", "Manager"
+        EMPLOYEE = "employee", "Employee"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.EMPLOYEE,
+    )
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username
--- backend/project/settings.py
+++ backend/project/settings.py
@@ -140,3 +140,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+AUTH_USER_MODEL = "app.User"
