b03037cc9cda48e64be94cc02be34eb4c95bf552
NikTak777 <nikkondrakhin@gmail.com>
2026-07-04 02:06:26 +0300

    Back-9 Добавлен маршрут /api/comments/

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import MeView, RegisterView, TaskViewSet, ProgressViewSet
+from .views import MeView, RegisterView, TaskViewSet, ProgressViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register("tasks", TaskViewSet, basename="task")
 router.register("progress", ProgressViewSet, basename="progress")
+router.register("comments", CommentViewSet, basename="comment")
 
 urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
