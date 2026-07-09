c5a8dcaba43bc21e39812b41c36203184e86c230
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-07-10 02:37:34 +0300

    Back-11 Add url for employees list endpoint

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("auth/me/", MeView.as_view(), name="me"),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("employees/", EmployeeListView.as_view(), name="employees"),
 ] + router.urls
